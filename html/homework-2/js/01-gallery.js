7dfac1c00b6348e09d7e9d27d5d22dece58bd2e3
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-02-04 17:14:45 +0200

    first part of homework-2 is done

--- html/homework-2/js/01-gallery.js
+++ html/homework-2/js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+galleryRef.addEventListener('click', modalWindowHandler);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+    `;
+    })
+    .join('');
+}
+
+function modalWindowHandler(e) {
+  e.preventDefault();
+
+  const isImgEl = e.target.classList.contains('gallery__image');
+
+  if (!isImgEl) {
+    return;
+  }
+
+  const modalWindow = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+  );
+
+  modalWindow.show();
+
+  document.addEventListener('keydown', e => {
+    if (!modalWindow.visible()) {
+      return;
+    }
+    if (e.code === 'Escape') {
+      modalWindow.close();
+    }
+  });
+}
